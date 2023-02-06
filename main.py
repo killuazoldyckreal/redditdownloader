faa3885f4ac281e24160126b44ae937ee8b6db4c
Killua_Zoldyck <58257876+killuazoldyckreal@users.noreply.github.com>
2023-02-07 00:47:50 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import requests
+import random
+import json
+import yt_dlp
+def save_meme(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/random/.json"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get(url, headers=headers)
+    data = json.loads(response.text)
+    if data[0]["kind"] == "Listing":
+        if data[0]["data"]["children"]!=[]:
+            random_post = data[0]["data"]["children"][0]["data"]
+            if "url" in random_post and random_post["url"].endswith(("jpg", "jpeg", "png")): #add .mp4 extension to include video posts
+                url= random_post["url"]
+                post_caption = random_post["title"]
+                ydl_opts_start = {
+                    'format': 'bv+ba/b',        #set 'format': 'wv+wa/w` to get lowest filesize
+                    'outtmpl': f'videos/%(title)s.%(ext)s',
+                    'no_warnings': False,
+                    'logtostderr': False,
+                    'ignoreerrors': False,
+                    'noplaylist': True,
+                    'writethumbnail': False
+                    }
+                ydl = yt_dlp.YoutubeDL(ydl_opts_start)
+                with ydl:
+                    result = ydl.extract_info(url, download=False)
+                    title = ydl.prepare_filename(result)
+                ydl.download([url])
+
+subreddit = "fingmemes" # Replace with desired subreddit name
+save_meme(subreddit)
